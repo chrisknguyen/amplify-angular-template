5f296d03fde0d1c6aac576a977173aed503f2030
Chris <chrisk.developer@gmail.com>
2025-06-12 14:51:24 -0700

    Included navbar and home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import {NavbarComponent} from "./features/shared/navbar/navbar.component";
 
 Amplify.configure(outputs);
 
@@ -11,7 +12,7 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent, NavbarComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
