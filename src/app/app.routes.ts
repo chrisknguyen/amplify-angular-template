65e0c69e8baaa58090d1f6270e0a09efeb3fd50d
Chris <chrisk.developer@gmail.com>
2025-06-13 15:40:37 -0700

    Included inquire page and some content.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomepageComponent} from "./features/homepage/homepage.component";
+import {TodosComponent} from "./todos/todos.component";
+import {ContactComponent} from "./features/contact/contact.component";
+import {InquireComponent} from "./features/inquire/inquire.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomepageComponent},
-];
+  {path: 'inquire', component: InquireComponent},
+  {path: 'todo', component: TodosComponent},
+  {path: 'contact', component: ContactComponent}
+]
---
+++ src/app/features/inquire/inquire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inquire',
+  standalone: true,
+  imports: [],
+  templateUrl: './inquire.component.html',
+  styleUrl: './inquire.component.css'
+})
+export class InquireComponent {
+
+}
